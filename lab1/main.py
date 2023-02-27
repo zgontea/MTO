df644b6a4dfb67b52e09dd52e2a4625294d06319
zgontea <228737@edu.p.lodz.pl>
2023-02-27 17:56:33 +0100

    update function logic

--- lab1/main.py
+++ lab1/main.py
@@ -2,16 +2,16 @@
 
 import sys
 
-def my_printf(format_string,param):
+def my_printf(format_string, param):
     #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
+    shouldDo = True
+    for idx in range(0, len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
+                print(param.swapcase(), end = "")
+                shouldDo = False
             else:
-                print(format_string[idx],end="")
+                print(format_string[idx].swapcase(), end = "")
         else:
             shouldDo=True
     print("")
