2ef489a7f93eac5fdca0fa336d0d92f7f60efc3b
zgontea <228737@edu.p.lodz.pl>
2023-03-06 14:26:40 +0100

    lab2 add string len logic

--- lab2/main.py
+++ lab2/main.py
@@ -4,19 +4,28 @@ import sys
 
 def my_printf(format_string, param):
     #print(format_string)
-    shouldDo = True
+    shouldDo = 0
+    stringMaxLen = 0
     for idx in range(0, len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+        currCharacter = format_string[idx]
+        if shouldDo == 0:
+            if currCharacter == '#' and format_string[idx + 1] == 'k':
                 print(param.swapcase(), end = "")
-                shouldDo = False
+                shouldDo += 1
+            elif currCharacter == '#' and format_string[idx + 1] == '.' and format_string[idx + 2].isnumeric() and format_string[idx + 3] == 'k':
+                stringMaxLen = int(format_string[idx + 2])
+                print(param.swapcase()[:stringMaxLen], end = "")
+                shouldDo += 3
             else:
-                print(format_string[idx].swapcase(), end = "")
+                print(currCharacter.swapcase(), end = "")
         else:
-            shouldDo=True
+            if shouldDo > 0:
+                shouldDo -= 1
+            else:
+                shouldDo = 0
     print("")
 
-data=sys.stdin.readlines()
+data = sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(),data[i + 1].rstrip())
